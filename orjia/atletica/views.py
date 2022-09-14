704f77fc39d797857e592b06281398fe19ffd334
Italo Oliveira <38302009+arkanttus@users.noreply.github.com>
2022-09-14 00:29:58 -0500

    feat: add atletica details (#2)

--- orjia/atletica/views.py
+++ orjia/atletica/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .forms import AtletaForm
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
+from .forms import AtletaForm
 from .forms import AtleticaForm
 from .models import Atletica
 
@@ -17,9 +18,14 @@ def atleta_add(request):
     return render(request, 'atleta_form.html', {'form': form})
 
 
-class RegisterView(SuccessMessageMixin, CreateView):
+class AtleticaCreate(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     template_name = 'forms/atletica_form.html'
     model = Atletica
     form_class = AtleticaForm
     success_message = '%(nome)s cadastrado com sucesso'
     success_url = reverse_lazy('base:index')
+
+
+class AtleticaDetail(LoginRequiredMixin, SuccessMessageMixin, DetailView):
+    model = Atletica
+    template_name: str = 'atletica/atletica_detail.html'
