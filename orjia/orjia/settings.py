23e46aa06f5d9f537ca7e2d6b6cc1d5c48076610
Italo Oliveira <italo.oliveira300@hotmail.com>
2023-03-20 21:43:51 -0500

    update allowed hosts

--- orjia/orjia/settings.py
+++ orjia/orjia/settings.py
@@ -23,11 +23,11 @@ PRODUCTION = True if env("PRODUCTION") == "True" else False
 
 # Allowed hosts based on current environment
 if PRODUCTION:
-    ALLOWED_HOSTS = [""]
-    CSRF_TRUSTED_ORIGINS = ["https://*.fly.dev"]
+    ALLOWED_HOSTS = ["127.0.0.1", "localhost", "liga.fly.dev"]
+    CSRF_TRUSTED_ORIGINS = ["https://liga.fly.dev"]
 else:
     ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
-    CSRF_TRUSTED_ORIGINS = ["https://*.fly.dev"]
+    CSRF_TRUSTED_ORIGINS = ["https://liga.fly.dev"]
 
 # Application definition
 
